4508e6303f631976eb35aa4708c361bd783e6879
Adriano Vieira <tsiadriano@gmail.com>
2024-04-21 22:22:01 -0300

    [FEAT] add some validations

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { api } from "./services/api"
 import moment from "moment"
 import Swal from 'sweetalert2'
 import withReactContent from 'sweetalert2-react-content'
+import { AxiosError } from 'axios'
 
 interface TaskProps{
   id: string;
@@ -34,113 +35,164 @@ export default function App() {
   }
 
   async function handleSubmit(event: FormEvent){
-    event.preventDefault();
-    const dueDate = moment(dueDateRef.current?.value).format('YYYY-MM-DDTHH:mm:ss.SSSZ')
-    const response = await api.post("/task",{
-      taskName: nameTaskRef.current?.value,
-      dueDate: dueDate,
-      priority: priorityRef.current?.value
-    })
+    const MySwal = withReactContent(Swal)
+    try{
+      event.preventDefault();
+      if(!nameTaskRef.current?.value || !dueDateRef.current?.value || !priorityRef.current?.value){
+        return  MySwal.fire({
+          title: "Error!",
+          html: `<span>Something is wrong.</span><br><span>Check that you have filled in all the fields</span>`,
+          icon: "error"
+        });
+      }
 
-    setTasks(allTasks => [...allTasks, response.data])
+      const dueDate = moment(dueDateRef.current?.value).format('YYYY-MM-DDTHH:mm:ss.SSSZ')
+      const response = await api.post("/task",{
+        taskName: nameTaskRef.current?.value,
+        dueDate: dueDate,
+        priority: priorityRef.current?.value
+      })
+      setTasks(allTasks => [...allTasks, response.data])
+
+      nameTaskRef.current.value = ""
+      dueDateRef.current.value = ""
+      priorityRef.current.value = ""
+    } catch (error: unknown) {
+      if (error instanceof AxiosError) {
+        MySwal.fire({
+          title: "Error!",
+          html: `<span>Something is wrong.</span><br> ${error.response?.data.message}`,
+          icon: "error"
+        });
+      }
+    }
   }
 
   async function deleteTask(id: string){
     const MySwal = withReactContent(Swal)
-    MySwal.fire({
-      title: "Are you sure?",
-      text: "You won't be able to revert this!",
-      icon: "warning",
-      showCancelButton: true,
-      confirmButtonColor: "#3085d6",
-      cancelButtonColor: "#d33",
-      confirmButtonText: "Yes, delete it!"
-    }).then(async (result) => {
-      if (result.isConfirmed) {
-        await api.delete("/task", {
-          params: {
-            id: id,
-          }
-        })
-
-        const allTasks = tasks.filter((task)=> task.id !== id)
-        setTasks(allTasks)
-        Swal.fire({
-          title: "Deleted!",
-          text: "Your file has been deleted.",
-          icon: "success"
+    try {
+      MySwal.fire({
+        title: "Are you sure?",
+        text: "You won't be able to revert this!",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "Yes, delete it!"
+      }).then(async (result) => {
+        if (result.isConfirmed) {
+          await api.delete("/task", {
+            params: {
+              id: id,
+            }
+          })
+  
+          const allTasks = tasks.filter((task)=> task.id !== id)
+          setTasks(allTasks)
+          Swal.fire({
+            title: "Deleted!",
+            text: "Your file has been deleted.",
+            icon: "success"
+          });
+        }
+      });
+    } catch (error: unknown) {
+      if (error instanceof AxiosError) {
+        MySwal.fire({
+          title: "Error!",
+          html: `<span>Something is wrong.</span><br> ${error.response?.data.message}`,
+          icon: "error"
+        });
+      } else {
+        MySwal.fire({
+          title: "Error!",
+          html: `<span>Something is wrong.</span><br>`,
+          icon: "error"
         });
       }
-    });
+    }
   }
 
   async function editTask(id: string, nameTask: string, dueDate: string, priority: string){
     const MySwal = withReactContent(Swal)
-    dueDate = moment(dueDate).format("YYYY-MM-DD")
-
-    MySwal.fire({
-      title: "<strong>Editar item</strong>",
-      html: `<form class="flex flex-col my-6">
-        <input id="id" type="text" class="hidden" value="${id}">
-        <label class="font-medium text-black text-left	">Name of Task:</label>
-        <input id="nameTask" class="w-full mb-5 p-2 rounded border-2 border-black-500" type="text" value="${nameTask}" ref={nameTaskRef} placeholder="Type the name of task" />
+    try {
+      dueDate = moment(dueDate).format("YYYY-MM-DD")
+      MySwal.fire({
+        title: "<strong>Editar item</strong>",
+        html: `<form class="flex flex-col my-6">
+          <input id="id" type="text" class="hidden" value="${id}">
+          <label class="font-medium text-black text-left	">Name of Task:</label>
+          <input id="nameTask" class="w-full mb-5 p-2 rounded border-2 border-black-500" type="text" value="${nameTask}" ref={nameTaskRef} placeholder="Type the name of task" />
+    
+          <div class="grid grid-cols-2 gap-x-2 mb-5">
+            <div class="text-left">
+              <label class="font-medium text-black">Due date</label>
+              <div>
+                <input id= "dueDate" class="w-full p-2 rounded h-10 border-2 border-black-500" type="date" ref={dueDateRef} value="${dueDate}" placeholder="Type the name of task" />
+              </div>
+            </div>
+          
+            <div class="text-left">
+              <label class="font-medium text-black">Priority</label>
+              <div>
+                <select id="priority" class="w-full rounded p-2 h-10 border-2 border-black-500" ref={priorityRef}>
+                  <option value="Normal" ${priority === "Normal" ? "selected" : ""}>Normal</option>
+                  <option value="High" ${priority === "High" ? "selected" : ""}>High</option>
+                  <option value="Urgent" ${priority === "Urgent" ? "selected" : ""}>Urgent</option>
+                </select>
+              </div>
+            </div>
+          </div>
+        </form>`,
+        showCloseButton: true,
+        showCancelButton: true,
+        focusConfirm: false,
+        confirmButtonText: `Save`,
+        confirmButtonAriaLabel: "Thumbs up, great!",
+        cancelButtonText: `Cancel`,
+        cancelButtonAriaLabel: "Thumbs down"
+      }).then(async (result) => {
+        if (result.isConfirmed) {
+          const id = document.getElementById('id') as HTMLInputElement
+          const nameTask = document.getElementById('nameTask') as HTMLInputElement;
+          const dueDateInput = document.getElementById('dueDate') as HTMLInputElement;
+          const dueDate = dueDateInput ? moment(dueDateInput.value).format('YYYY-MM-DDTHH:mm:ss.SSSZ') : null;
+          const priority = document.getElementById('priority') as HTMLSelectElement;
+          
+          const response = await api.post("/updateTask",{
+            id: id.value,
+            taskName: nameTask.value,
+            dueDate: dueDate,
+            priority: priority.value
+          })
   
-        <div class="grid grid-cols-2 gap-x-2 mb-5">
-          <div class="text-left">
-            <label class="font-medium text-black">Due date</label>
-            <div>
-              <input id= "dueDate" class="w-full p-2 rounded h-10 border-2 border-black-500" type="date" ref={dueDateRef} value="${dueDate}" placeholder="Type the name of task" />
-            </div>
-          </div>
-        
-          <div class="text-left">
-            <label class="font-medium text-black">Priority</label>
-            <div>
-              <select id="priority" class="w-full rounded p-2 h-10 border-2 border-black-500" ref={priorityRef}>
-                <option value="Normal" ${priority === "Normal" ? "selected" : ""}>Normal</option>
-                <option value="High" ${priority === "High" ? "selected" : ""}>High</option>
-                <option value="Urgent" ${priority === "Urgent" ? "selected" : ""}>Urgent</option>
-              </select>
-            </div>
-          </div>
-        </div>
-      </form>`,
-      showCloseButton: true,
-      showCancelButton: true,
-      focusConfirm: false,
-      confirmButtonText: `Save`,
-      confirmButtonAriaLabel: "Thumbs up, great!",
-      cancelButtonText: `Cancel`,
-      cancelButtonAriaLabel: "Thumbs down"
-    }).then(async (result) => {
-      if (result.isConfirmed) {
-        const id = document.getElementById('id') as HTMLInputElement
-        const nameTask = document.getElementById('nameTask') as HTMLInputElement;
-        const dueDateInput = document.getElementById('dueDate') as HTMLInputElement;
-        const dueDate = dueDateInput ? moment(dueDateInput.value).format('YYYY-MM-DDTHH:mm:ss.SSSZ') : null;
-        const priority = document.getElementById('priority') as HTMLSelectElement;
-        
-        console.log(nameTask.value)
-        console.log(dueDate)
-        console.log(priority.value)
-        const response = await api.post("/updateTask",{
-          id: id.value,
-          taskName: nameTask.value,
-          dueDate: dueDate,
-          priority: priority.value
-        })
-
-        setTasks(allTasks => {
-          const updatedTasks = allTasks.map(task => {
-            if (task.id === response.data.id) {
-              return response.data;
-            }
-            return task;
+          setTasks(allTasks => {
+            const updatedTasks = allTasks.map(task => {
+              if (task.id === response.data.id) {
+                return response.data;
+              }
+              return task;
+            });
+            return updatedTasks;
           });
-          return updatedTasks;
+        }
+      });
+    } catch (error: unknown) {
+      if (error instanceof AxiosError) {
+        MySwal.fire({
+          title: "Error!",
+          html: `<span>Something is wrong.</span><br> ${error.response?.data.message}`,
+          icon: "error"
+        });
+      } else {
+        MySwal.fire({
+          title: "Error!",
+          html: `<span>Something is wrong.</span><br>`,
+          icon: "error"
         });
       }
-    });
+    }
+
   }
 
 
